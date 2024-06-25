5e0b3aa4d3808a1bd32229d37eabd12f9326ca06
haykmanasyan <cloudstatic01@gmail.com>
2024-06-25 16:04:40 -0400

    add view.html to see scans

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, url_for, send_from_directory
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # List .nii files in the src directory
+    src_folder = 'src'
+    files = [f for f in os.listdir(src_folder) if f.endswith('.nii')]
+    return render_template('index.html', files=files)
+
+@app.route('/view')
+def view():
+    # Get the selected file from the query parameters
+    file_name = request.args.get('file')
+    if file_name:
+        file_path = os.path.join('src', file_name)
+        if os.path.exists(file_path):
+            return render_template('view.html', file_name=file_name)
+    return "File not found", 404
+
+@app.route('/src/<filename>')
+
+def serve_file(filename):
+    return send_from_directory('src', filename)
+
+if __name__ == '__main__':
+
+    app.run(debug=True, host='0.0.0.0', port=8080)
